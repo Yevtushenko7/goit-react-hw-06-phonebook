76a534d529fc3cf7cd1ab8709706ce81cc7bdc17
Yevtushenko7 <71986994+Yevtushenko7@users.noreply.github.com>
2021-07-17 00:05:06 +0300

    Update Filter.js

--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -1,25 +1,3 @@
-// import React from "react";
-// import PropTypes from "prop-types";
-// import styles from "../Filter/Filter.module.css";
-
-// const Filter = ({ value, onFilter }) => {
-//   return (
-//     <input
-//       className={styles.input}
-//       type="text"
-//       value={value}
-//       onChange={onFilter}
-//     />
-//   );
-// };
-
-// Filter.propTypes = {
-//   value: PropTypes.string.isRequired,
-//   onFilter: PropTypes.func.isRequired,
-// };
-
-// export default Filter;
-
 import React from "react";
 import { connect } from "react-redux";
 import styles from '../Filter/Filter.module.css';
